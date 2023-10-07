045625610bd2f8112407506dc35d9188064483e3
Ninja Solution <top1successfulman@gmail.com>
2023-10-08 08:04:42 +1000

    update

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-var RIPEMD160 = require('ripemd160')
 require('dotenv').config();
-const { ethers } = require("ethers");
 
 var service = require("./service")
 
-// service.startBot(5)
-
 if(process.argv.length > 2) {
-    console.log(`Source: ${process.argv[2]}`)
-    console.log(`Result Hash: ${new RIPEMD160().update(process.argv[2]).digest('hex')}`)
+    service.startBot(Number.parseInt(process.argv[2]))
 }
