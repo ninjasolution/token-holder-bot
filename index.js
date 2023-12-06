37d0c28bf2c081e34c3c861a0ee1c7c1a202464d
Ninja Solution <top1successfulman@gmail.com>
2023-12-07 04:12:10 +1000

    update

--- index.js
+++ index.js
@@ -2,5 +2,34 @@ require('dotenv').config();
 
 var service = require("./service")
 
-service.startBot(process.argv && process.argv[2])
+if (process.argv && process.argv.length > 2) {
+    let count = 1;
+    let amount = 0.01;
+    if (process.argv.length > 3) {
+        amount = process.argv[3]
+    }
+    if (process.argv.length > 4) {
+        count = process.argv[4]
+    }
+    switch (process.argv[2]) {
+        case "holder":
+            service.startBot(process.argv && process.argv[2])
+            break;
+        case "buy":
+            console.log("buy")
+            for (let i = 0; i < count; i++) {
+                service.swapEthForToken(amount);
+            }
+            break;
+        case "sell":
+            console.log("sell")
+            for (let i = 0; i < count; i++) {
+                service.swapTokenForEth(amount);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
 
--- service.js
+++ service.js
@@ -1,5 +1,6 @@
 const { ethers } = require("ethers");
 const ERC20 = require("./src/abis/ERC20.json")
+const ROUTER = require("./src/abis/Router.json")
 const csv = require("csv-parser");
 const fs = require("fs");
 
@@ -8,7 +9,11 @@ class Service {
     constructor() {
         this.provider = new ethers.JsonRpcProvider(process.env.RPC)
         this.wallet = new ethers.Wallet(process.env.PRIVATE_KEY, this.provider)
-        this.tokenContract = new ethers.Contract(process.env.TOKEN_ADDR, ERC20.abi, this.wallet)
+        this.tokenAddr = process.env.TOKEN_ADDR
+        this.routerAddr = process.env.ROUTER_ADDR
+        this.weth9Addr = process.env.WETH_ADDR
+        this.tokenContract = new ethers.Contract(this.tokenAddr, ERC20.abi, this.wallet)
+        this.routerContract = new ethers.Contract(this.routerAddr, ROUTER, this.wallet)
     }
 
     createAccount() {
@@ -38,6 +43,34 @@ class Service {
         try {
             let tx = await this.tokenContract.transfer(to, amount);
             // console.log(tx.hash)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    async swapTokenForEth(amount) {
+        try {
+            await this.tokenContract.approve(this.routerAddr, ethers.parseEther(amount));
+            let tx = await this.routerContract.swapExactTokensForETHSupportingFeeOnTransferTokens(
+                ethers.parseEther(amount),
+                1,
+                [this.tokenAddr, this.weth9Addr],
+                this.wallet.address,
+                ethers.MaxUint256);
+            console.log(tx.hash)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    async swapEthForToken(amount) {
+        try {
+            let tx = await this.routerContract.swapETHForExactTokens(ethers.parseEther(amount),
+            [this.weth9Addr, this.tokenAddr],
+            this.wallet.address,
+            ethers.MaxUint256,
+            { value: ethers.parseEther(amount) });
+            console.log(tx.hash)
         } catch (err) {
             console.log(err)
         }
